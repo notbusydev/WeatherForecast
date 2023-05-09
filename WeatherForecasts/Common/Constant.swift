18a41c87db18a511367742ddb9b0e4eb5590e42a
jaebin <morebgle@gmail.com>
2023-05-09 09:50:39 +0900

    수정: API Key

---
+++ WeatherForecasts/Common/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  WeatherForecasts
+//
+//  Created by JaeBin on 2023/05/09.
+//
+
+import Foundation
+
+struct Constant {
+    
+    static let apiKey = ""
+    static let apiHost = "https://api.openweathermap.org"
+}
